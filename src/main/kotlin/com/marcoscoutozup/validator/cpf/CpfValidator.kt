c95db00b3051b17678edb54624ae43e624fccad3
Marcos Couto <marcos.couto@zup.com.br>
2020-11-26 20:44:02 -0300

    Dockerfile multi-stage build

--- src/main/kotlin/com/marcoscoutozup/validator/cpf/CpfValidator.kt
+++ src/main/kotlin/com/marcoscoutozup/validator/cpf/CpfValidator.kt
@@ -7,5 +7,5 @@ import io.micronaut.validation.validator.constraints.ConstraintValidatorContext
 class CpfValidator: ConstraintValidator<Cpf, String> {
 
     override fun isValid(value: String?, annotationMetadata: AnnotationValue<Cpf>, context: ConstraintValidatorContext): Boolean =
-            value != null && value?.matches(Regex("^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}\$"))
+            value != null && value.matches(Regex("^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}\$"))
 }
