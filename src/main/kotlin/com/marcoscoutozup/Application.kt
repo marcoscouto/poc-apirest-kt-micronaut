76e1858b7101902f023901e43d19ca85fa9d2a0f
Marcos Couto <marcosmartinellicouto@gmail.com>
2020-12-09 19:27:08 -0300

    Adicionando OpenAPI

--- src/main/kotlin/com/marcoscoutozup/Application.kt
+++ src/main/kotlin/com/marcoscoutozup/Application.kt
@@ -1,10 +1,30 @@
 package com.marcoscoutozup
 
 import io.micronaut.runtime.Micronaut.*
-fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("com.marcoscoutozup")
-		.start()
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+
+@OpenAPIDefinition(
+	info = Info(
+		title = "Api Rest com Kotlin e Micronaut",
+		version = "1.0",
+		description = "Demonstração de Api Rest com Kotlin e Micronaut",
+		license = License(name = "Licença 1.0", url="https://github.com/marcoscouto"),
+		contact = Contact(url = "https://github.com/marcoscouto", name = "Marcos Couto", email = "marcosmartinellicouto@gmail.com")
+	)
+)
+object Application {
+
+	@JvmStatic
+	fun main(args: Array<String>) {
+		build()
+			.args(*args)
+			.packages("com.marcoscoutozup")
+			.start()
+	}
+
 }
+
 
